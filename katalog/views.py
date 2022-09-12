fc4e48229f06f8814c031948e6d1df74a1ced779
frestytania <frestytania@gmail.com>
2022-09-12 12:03:45 +0700

    push and deploy app

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(req):
+    return render(req, "katalog.html", context)
+
+item_data = CatalogItem.objects.all()
+context = {
+    'item_list': item_data,
+    'nama': 'Fresty',
+    'npm': '2106750742'
+}
