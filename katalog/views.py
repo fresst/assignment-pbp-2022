ef3b1b760d92eb19651d1b934cb52354e65aa909
frestytania <frestytania@gmail.com>
2022-09-14 21:53:49 +0700

    updating katalog/views.py

--- katalog/views.py
+++ katalog/views.py
@@ -3,11 +3,10 @@ from katalog.models import CatalogItem
 
 # TODO: Create your views here.
 def show_catalog(req):
+    item_data = CatalogItem.objects.all()
+    context = {
+        'item_list': item_data,
+        'nama': 'Fresty',
+        'npm': '2106750742'
+    }
     return render(req, "katalog.html", context)
-
-item_data = CatalogItem.objects.all()
-context = {
-    'item_list': item_data,
-    'nama': 'Fresty',
-    'npm': '2106750742'
-}
